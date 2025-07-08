89366df8a9fbb34dd8418e7135ec2b144ff5fd73
MANIK <manik.sd10307@gmail.com>
2025-07-08 13:43:27 +0530

    Time: 272 ms (57.78%), Space: 0B (100%) - LeetHub

---
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -0,0 +1,11 @@
+WITH
+  RankedEmployees AS (
+    SELECT *, DENSE_RANK() OVER(ORDER BY salary DESC) AS `rank`
+    FROM Employee
+  )
+SELECT MAX(salary) AS SecondHighestSalary
+FROM RankedEmployees
+WHERE `rank` = 2;
+
+
+
